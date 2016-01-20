939e9ddc34a1f8ce27ca2f7b258479acc35fbdd6
Anveshan <akunduru4@gmail.com>
2016-01-20 06:41:49 +0530

    Search checkin

---
+++ Algorithms/search/InsertionSort1.java
@@ -0,0 +1,31 @@
+package search;
+
+import java.util.Arrays;
+
+public class InsertionSort1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numbers = {34,21,78,90,4,57};
+		System.out.println("Before Sort :"+Arrays.toString(numbers));
+		sortInts(numbers);
+		System.out.println("After Sort :"+Arrays.toString(numbers));
+	}
+
+	
+	public static void sortInts(int[] nums){
+		int temp;
+		for(int i = 1; i < nums.length; i++){
+			for( int j = i; j > 0 ; j--){ //Second differentiates the performance of the sort.it executes less for already sorted
+				if(nums[j] < nums[j-1]){
+					temp = nums[j];
+					nums[j] = nums[j-1];
+					nums[j-1] = temp;
+				}
+				else 
+					break;
+			}
+		}
+	}
+}
---
+++ Algorithms/search/MergeSort1.java
@@ -0,0 +1,72 @@
+package search;
+
+import java.util.Arrays;
+
+public class MergeSort1 {
+
+	private static int[] nums =  {9,8,7,6,10,5,4,3,2,1};   //7,3,9,4,8,2,6,1 {4,9}; //
+	//private static  int[] nums = {3,2,1};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Before Merge "+Arrays.toString(nums));
+		long startTime = System.nanoTime();
+		mergeSort(0,nums.length-1);
+		long endTime = System.nanoTime();
+		System.out.println("After Merge "+Arrays.toString(nums));
+		System.out.println("The time taken to sort is "+(endTime - startTime) +" nano seconds");
+	}
+
+	
+	private static void mergeSort(int low,int high){
+		if(low >= high)
+			return ;
+		else {
+			int mid = (low + high)/2 ;
+			mergeSort(low,mid);
+			mergeSort(mid+1,high);
+			mergeArrays(low,mid,high);
+		}
+	}
+	
+	private static void mergeArrays(int low, int mid, int high){
+		int[] tempArr = new int[(high - low)+1];
+		
+		for(int i = low, j = mid+1, b = 0; ;){
+			if(nums[i] < nums[j]){
+				tempArr[b] = nums[i];b++;
+				if(i < mid){
+					i++;
+				}
+				else {
+					while(j <= high){
+						tempArr[b] = nums[j];j++;b++;
+					}
+				}
+				
+			}
+			else {
+				tempArr[b] = nums[j];b++;
+				if(j < high){
+					j++;
+				}
+				else {
+					while(i <= mid){
+						tempArr[b] = nums[i];i++;b++;
+					}
+				}
+			}
+			
+			if(b == tempArr.length){
+				break;
+			}
+		}
+		int i = 0;
+		while(low <= high){
+			nums[low] = tempArr[i];
+			low++;
+			i++;
+		}
+	}
+}
