92be696aba58330b942669afbdfd8b47b20a11bb
Anveshan <akunduru4@gmail.com>
2016-02-20 17:21:59 +0530

    Insertion sort check in

---
+++ coursera-Algorithms/sort/InsertionSort.java
@@ -0,0 +1,20 @@
+package sort;
+
+public class InsertionSort {
+
+	@SuppressWarnings(value = { "rawtypes" })
+	public static void sort(Comparable[] elements){
+		
+		for(int i = 1; i < elements.length; i++){
+			for(int j = i; j > 0; j--){
+				if(CallbackImpl.less(elements[j], elements[j-1])){
+					CallbackImpl.exchange(elements, j, j-1);
+				}
+				else {
+					break;
+				}
+			}
+			
+		}
+	}
+}
